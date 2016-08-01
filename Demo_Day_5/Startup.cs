3489c7b372220b87de45a9018ee6bccb8633b3d4
jojuo123 <vtduy3100@gmail.com>
2016-08-01 20:34:47 +0700

    commited

---
+++ Demo_Day_5/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Demo_Day_5.Startup))]
+namespace Demo_Day_5
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
